835a4b9db732cdaa1036e3f92236e1383c9171bf
edwardsun007 <edwardsun007@gmail.com>
2021-02-24 21:47:39 -0800

    OAuth got work and get API works after authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Games } from "./Components/Games";
+import { Streams } from "./Components/Streams";
+import { Header } from "./Components/Headers";
+import { NotFound } from "./Components/NotFound";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+
+// import "./App.css";
+
+const App = (): React.ReactElement => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Games} />
+          <Route exact path="/top-streams" component={Streams} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
